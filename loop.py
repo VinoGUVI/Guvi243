bdee1458dfa7b51d8f066e3a923db3876c3ed4b4
VinoGUVI <vinodha@guvi.in>
2024-06-19 19:44:37 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,13 @@
+#User Defined Function
+
+#function definition - return
+def sum(a,b=10): #parameters
+    c=a+b
+    return c
+
+#Function calling
+a=int(input("Enter "))
+b=int(input("Enter "))
+print(sum(a,b))#Arguments
+
+print(print("Hello"))
